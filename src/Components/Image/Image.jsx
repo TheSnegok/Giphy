b80dea76f90cb1e4dc8bfa5ad7a938b0c1178de4
Max <lord180499@gmail.com>
2021-06-30 18:42:16 +0300

    loader fix

---
+++ src/Components/Image/Image.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import s from "./Image.module.css";
+
+const Image = (props) => {
+	let gif = props.gif;
+
+	const [loader, setLoader] = useState(false);
+
+	let clickCopy = (elem) => {
+		navigator.clipboard.writeText(elem);
+	};
+
+	const loadImage = () => {
+		setLoader(true);
+	}
+
+	return (
+		<div className={s.wrapper} key={gif.id}>
+			<img
+				src={gif.images.fixed_height.webp}
+				alt={gif.slug}
+				className={!loader ? s.gifNone : s.gif}
+				onClick={() => clickCopy(gif.images.original.url)}
+				onLoad={() => loadImage()}
+			/>
+			{!loader && <div className={s.loader}></div>}
+		</div>
+	);
+};
+
+export default Image;
--- src/Components/Showgif/Showgif.jsx
+++ src/Components/Showgif/Showgif.jsx
@@ -1,35 +1,9 @@
-import { useEffect, useState } from "react";
-import s from "./Showgif.module.css";
+import Image from "../Image/Image";
 
 const Showgif = (props) => {
 	let gif = props.gifs;
 
-	const [loader, setLoader] = useState(false);
-
-	useEffect(() => {
-		setLoader(false);
-	}, [props.gifs]);
-
-	const loadImage = () => {
-		setLoader(true);
-	};
-
-	let clickCopy = (elem) => {
-		navigator.clipboard.writeText(elem);
-	};
-
-	return gif.map((gif, number) => (
-		<div className={s.wrapper} key={gif.id}>
-			<img
-				src={gif.images.fixed_height.webp}
-				alt={gif.slug}
-				className={!loader ? s.gifNone : s.gif}
-				onClick={() => clickCopy(gif.images.original.url)}
-				onLoad={() => loadImage()}
-			/>
-			{!loader && <div className={s.loader}></div>}
-		</div>
-	));
+	return gif.map((gif) => <Image gif={gif} />);
 };
 
 export default Showgif;
