3ca5f249f24a37dc31837812b7b91f6e33bc32eb
Max <lord180499@gmail.com>
2021-07-01 13:18:49 +0300

    onLoad img work

--- src/redux/reducers/gifReducer.js
+++ src/redux/reducers/gifReducer.js
@@ -1,14 +1,14 @@
 import { gipherAPI } from "../../api/api";
 
 const SAVE_GIF = "SAVE_GIF",
-	  SET_LANG = "SET_LANG",
-	  SET_OFFSET = "SET_OFFSET",
-	  SAVE_NEW_GIF = "SAVE_NEW_GIF";
+	SET_LANG = "SET_LANG",
+	SET_OFFSET = "SET_OFFSET",
+	SAVE_NEW_GIF = "SAVE_NEW_GIF";
 
 const initialState = {
 	items: [],
 	language: "en",
-	offset: 0
+	offset: 0,
 };
 
 let gifReducer = (state = initialState, action) => {
@@ -16,22 +16,22 @@ let gifReducer = (state = initialState, action) => {
 		case SAVE_GIF:
 			return {
 				...state,
-				items: action.gif
+				items: action.gif,
 			};
 		case SAVE_NEW_GIF:
 			return {
 				...state,
-				items: [...state.items, ...action.newGif]
+				items: [...state.items, ...action.newGif],
 			};
 		case SET_LANG:
 			return {
 				...state,
-				language: action.lang
+				language: action.lang,
 			};
 		case SET_OFFSET:
 			return {
 				...state,
-				offset: action.offset
+				offset: action.offset,
 			};
 		default:
 			return state;
@@ -41,21 +41,23 @@ let gifReducer = (state = initialState, action) => {
 export const saveGif = (gif) => ({ type: SAVE_GIF, gif });
 export const saveNewGif = (newGif) => ({ type: SAVE_NEW_GIF, newGif });
 export const setLanguage = (lang) => ({ type: SET_LANG, lang });
-export const setOffset = (offset) => ({ type: SET_OFFSET, offset});
+export const setOffset = (offset) => ({ type: SET_OFFSET, offset });
 
-export const giveGif = (text, lang, offset = 1) => async (dispatch) => {
-	let response = await gipherAPI.getGif(text, lang, offset);
-	if (response.meta.status === 200) {
-		dispatch(saveGif(response.data));
-		dispatch(setOffset(offset+15));
-	}
-};
+export const giveGif =
+	(text, lang, offset = 1) =>
+	async (dispatch) => {
+		let response = await gipherAPI.getGif(text, lang, offset);
+		if (response.meta.status === 200 && response.data.length > 0) {
+			dispatch(saveGif(response.data));
+			dispatch(setOffset(offset + 15));
+		}
+	};
 
 export const giveMoreGif = (text, lang, offset) => async (dispatch) => {
 	let response = await gipherAPI.getGif(text, lang, offset);
 	if (response.meta.status === 200) {
 		dispatch(saveNewGif(response.data));
-		dispatch(setOffset(offset+15));
+		dispatch(setOffset(offset + 15));
 	}
 };
 
